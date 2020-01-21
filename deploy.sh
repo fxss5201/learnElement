294b7c9976d4bc82e28cebe50216beae1cd5a1bf
fys <723107234@qq.com>
2020-01-21 16:59:28 +0800

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://fxss5201.github.io/learnElement
+git push -f git@github.com:fxss5201/learnElement.git master:gh-pages
 
 cd -
